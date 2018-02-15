199d7dc3fb068dd2bca58fd88c9623f1c4f73ea9
platinum <payplatinum@outlook.com>
2018-02-15 23:02:52 +0500

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -1212,8 +1212,10 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
                fprintf(ConfFile, "rpcport=10257\n");
                fprintf(ConfFile, "rpcallowip=127.0.0.1\n");
                fprintf(ConfFile, "addnode=127.0.0.1\n");
-               fprintf(ConfFile, "addnode=37.1.213.58\n");
-               fprintf(ConfFile, "addnode=194.87.237.90\n");
+               fprintf(ConfFile, "addnode=plt.cash\n");
+               fprintf(ConfFile, "addnode=pool.plt.cash\n");
+               fprintf(ConfFile, "addnode=78.29.45.112\n");
+               fprintf(ConfFile, "addnode=block.plt.cash\n");
 
 
                fclose(ConfFile);
