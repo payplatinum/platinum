089cb4cabade919989baba6db72ab3902b25b4ea
platinum <payplatinum@outlook.com>
2018-02-20 09:10:10 +0500

    CPU Miner for Platinum
    This script is for a wallet "Platinum". To enable mining complite script minerrun.sh
    as a root user. If you haven't root, open shell and type "sudo passwd root",
    and set any password(DON'T FORGET THAT PASS).
    That script setup Platinum-cpuminer and make it autorun anytime after boot your system.

---
+++ CPUminerrun.sh
@@ -0,0 +1,39 @@
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+git clone https://github.com/SazonovDV/cpuminer-multi
+cd cpuminer-multi
+U=$PWD
+./build.sh
+find / -name ".PLATINUM" > configs.txt
+cp cpuminer /usr/bin
+I=0
+while read LINE
+	do
+	I=$[I + 1]
+	US=`grep -E rpcuser ${LINE}/Platinum.conf | cut -d= -f2`
+	PS=`grep -E rpcpassword ${LINE}/Platinum.conf | cut -d= -f2`
+	touch /usr/bin/${US}${I}Run.sh
+	chmod 755 /usr/bin/${US}${I}Run.sh
+   		echo '#!/bin/sh' > /usr/bin/${US}${I}Run.sh
+   		echo cpuminer -a X11 -o http://127.0.0.1:10257 --userpass=${US}:${PS} >> /usr/bin/${US}${I}Run.sh
+	cd /lib/systemd/system
+	touch ${US}${I}Run.service
+   	chmod 755 ${US}${I}Run.service
+   	echo '[Unit]' > ${US}${I}Run.service
+   	echo 'Description=PlatinumAutorunScript' >> ${US}${I}Run.service
+   	echo 'After=multi-user.target' >> ${US}${I}Run.service
+
+   	echo '[Service]' >> ${US}${I}Run.service
+   	echo 'Type=simple' >> ${US}${I}Run.service
+   	echo 'User=root' >> ${US}${I}Run.service
+   	echo  ExecStart=/usr/bin/${US}${I}Run.sh >> ${US}${I}Run.service
+   	echo 'OOMScoreAdjust=-100' >> ${US}${I}Run.service
+	echo 'TimeoutSec=30' >> ${US}${I}Run.service
+   	echo '[Install]' >> ${US}${I}Run.service
+   	echo 'WantedBy=multi-user.target' >> ${US}${I}Run.service
+	systemctl enable ${US}${I}Run.service
+   	systemctl start ${US}${I}Run.service
+   	systemctl daemon-reload
+	cd $U
+done < configs.txt
+rm configs.txt
+
